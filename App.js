1f9886da41694dd7a44369ec744d780c66472ed6
Kunal Arora <kuarora@ucsd.edu>
2021-02-19 22:24:04 -0800

    .env file created

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import firebase from 'firebase'
 
+const config = require('./config');
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBNlOin_a4QCzVoWDm92h5vcwVaJja0Oqk",
-  authDomain: "zomo-d2bdd.firebaseapp.com",
-  projectId: "zomo-d2bdd",
-  storageBucket: "zomo-d2bdd.appspot.com",
+  apiKey: config.FIREBASE_APIKEY,
+  authDomain: config.FIREBASE_DOMAIN,
+  projectId: config.FIREBASE_ID,
+  storageBucket: config.FIREBASE_ID + ".appspot.com",
   messagingSenderId: "778399872549",
   appId: "1:778399872549:web:085b9771a080684eb2f1cc",
   measurementId: "G-82ZD86YPSZ"
