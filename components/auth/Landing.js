2ac1318adb000f7ebda1c553648b41f39226c716
Kunal Arora <kuarora@ucsd.edu>
2021-02-20 10:19:51 -0800

    oauth2 not working

--- components/auth/Landing.js
+++ components/auth/Landing.js
@@ -2,12 +2,20 @@ import React from 'react'
 import { Text, View, Button } from 'react-native'
 
 export default function Landing({ navigation }) {
+    const [accessToken,setAccessToken] = useState();
+    const [refreshToken,setRefreshToken] = useState();
+
+    const redirectSSO = e => {
+        setAccessToken(e.accessToken);
+        setRefreshToken(e.refreshToken);
+    }
+
     return (
         <View style={{ flex: 1, justifyContent: 'center' }}>
             <h2>Zomo</h2>
             <Button
                 title="Login using UCSD SSO"
-                onPress={() => navigation.navigate("login")} />
+                onPress={redirectSSO} />
         </View>
     )
 }
